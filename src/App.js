227c4cead83a60ff1132956527628be38f553a1d
elliot <elliotgcraig@icloud.com>
2019-02-06 13:24:50 -0700

    im going to try to get comp za to get z_cur from redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import './Reset.css';
+import {Provider} from 'react-redux';
+import store from './Ducks/store';
 
 import Header from './Components/Header/Header'
 import Body from './Components/Body/Body'
@@ -8,10 +10,12 @@ import Body from './Components/Body/Body'
 class App extends Component {
   render() {
     return (
-      <div className='page_one'>
-        <Header />
-        <Body />
-      </div>
+      <Provider store={store}>
+        <div className='page_one'>
+          <Header />
+          <Body />
+        </div>
+      </Provider>
     );
   }
 }
---
+++ src/Ducks/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    z_cur: 'z',
+}
+
+const reducer = (state = initialState, action)=>{
+    const {type, payload}=action
+    switch (type){
+        default:
+            return state
+    }
+}
+export default reducer
